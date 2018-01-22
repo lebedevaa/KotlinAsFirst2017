d2b6ee6ac774c77c1c2838d332fae5ee1634798b
lebedevaa <lebedeva.yasya@yandex.ru>
2018-01-22 23:56:34 +0300

    7

--- src/lesson7/task2/Matrices.kt
+++ src/lesson7/task2/Matrices.kt
@@ -103,8 +103,7 @@ fun generateSnake(height: Int, width: Int): Matrix<Int> = TODO()
  * 4 5 6      8 5 2
  * 7 8 9      9 6 3
  */
-fun <E> rotate(matrix: Matrix<E>): Matrix<E> = TODO()
-
+fun <E> rotate(matrix: Matrix<E>): Matrix<E>  = TODO()
 /**
  * Сложная
  *
